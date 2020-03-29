cc82ced8c2481355e3f754a4407e217be16ba809
Shashank Srivastava <shashank.ssriv@outlook.com>
2020-03-29 10:01:24 +0530

    Clean up code, add Google Analytics trackinger & deploy scripts

--- app.py
+++ app.py
@@ -35,18 +35,16 @@ def main():
         num_cases.append(i[2])
         country_states.append(i[1])
         num_casualties.append(i[5])
-        #num_people_cured.append(i[4])
 
     state_wise_stats.remove(state_wise_stats[-1])
     country_states.remove(country_states[-1])
     num_cases.remove(num_cases[-1])
     num_casualties.remove(num_casualties[-1])
-    #num_people_cured.remove(num_people_cured[-1])
 
     print(num_people_cured)
 
     for j in country_states:
-        if j   == country_states[-1]:
+        if j == country_states[-1]:
             break
     max = 0
     for j in num_cases:
@@ -60,33 +58,26 @@ def main():
     state_with_max_cases = country_states[max_value_index]
 
     max_casualties = 0
-    #max_casualties_value_index = 0
     for j in num_casualties:
         print(j[0])
-    print("last element", country_states[-1])    
+    print("last element", country_states[-1])
     for j in num_casualties:
-        #if j == num_casualties[-1]:
-            #break
         k = j
         k = int(k)
         if k >= max_casualties:
             max_casualties = k
-            print("max casualties..........inner block", max_casualties)
-            print("iteration number", j) 
             max_casualties_value_index = num_casualties.index(j)
-    print("max casualties", max_casualties)    
-    
+
     state_with_max_casualties = country_states[max_casualties_value_index]
-    print("state", state_with_max_casualties)
-    '''performance = []
-for row in stats :
-	performance.append(int(row[2]) + int(row[3]))
-#print(performance)'''
 
     return render_template('index.html', country_states=country_states,
-                           num_cases=num_cases, num_casualties=num_casualties, num_people_cured=num_people_cured,
-                           total_casualties=total_casualties, total_cases=total_cases, max=max, state_with_max_cases=state_with_max_cases,
-                           max_casualties=max_casualties, state_with_max_casualties=state_with_max_casualties)
+                           num_cases=num_cases, num_casualties=num_casualties,
+                           num_people_cured=num_people_cured,
+                           total_casualties=total_casualties,
+                           total_cases=total_cases,
+                           max=max, state_with_max_cases=state_with_max_cases,
+                           max_casualties=max_casualties,
+                           state_with_max_casualties=state_with_max_casualties)
 
 
 if __name__ == "__main__":
